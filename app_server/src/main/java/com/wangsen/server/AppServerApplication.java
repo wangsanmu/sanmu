301cecba5195f1fa0c4920cffa7342e9d392641c
wangsen <466348638@qq.com>
2018-04-06 11:15:14 +0800

    第一次提交

--- app_server/src/main/java/com/wangsen/server/AppServerApplication.java
+++ app_server/src/main/java/com/wangsen/server/AppServerApplication.java
@@ -1,4 +1,4 @@
-package com.wangsen.app_server;
+package com.wangsen.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- app_server/src/test/java/com/wangsen/server/AppServerApplicationTests.java
+++ app_server/src/test/java/com/wangsen/server/AppServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wangsen.app_server;
+package com.wangsen.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
