aa47e237da52ee0ba70aed889c0484e366880c36
wangsen <466348638@qq.com>
2018-04-06 11:13:41 +0800

    第一次提交

---
+++ src/main/java/com/wangsen/sanmu/SanmuApplication.java
@@ -0,0 +1,12 @@
+package com.wangsen.sanmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanmuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanmuApplication.class, args);
+	}
+}
